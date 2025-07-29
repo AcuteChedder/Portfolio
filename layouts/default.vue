76a7be1ad6668671ff52ed9b09c925f1f7176414
AcuteChedder <nubik8653@gmail.com>
2025-07-30 01:00:32 +0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+
+    <header>
+       
+    </header>
+    
+    <main>
+        <slot />
+    </main>
+
+    <footer>
+
+    </footer>
+
+</div>
+
+</template>
+
+<style>
+
+</style>
